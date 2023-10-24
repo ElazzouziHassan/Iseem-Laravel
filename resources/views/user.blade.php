96a31920013a8f64d8491c2e46872956b58e644a
wizardy <wizardhassan99@gmail.com>
2023-10-24 13:56:27 +0100

    commit

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,12 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>LARAVEL APP | USER PAGE</title>
-</head>
-<body>
-  <h1>WELCOME, USER PAGE: {{ $name }}</h1>
-</body>
-</html>
+@extends('layout')
+@section('title')
+  User Page
+@endsection()
+@section('content')
+  <h1>User Page</h1>
+@endsection
