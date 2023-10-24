e07f0458742a767c7b10e793909e996d317e72bd
wizardy <wizardhassan99@gmail.com>
2023-10-24 13:56:00 +0100

    commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>LARAVEL APP | HOME PAGE</title>
-</head>
-<body>
-  <h1>WELCOME, HOME PAGE</h1>
-</body>
-</html>
+@extends('layout')
+@section('title')
+  Home Page
+@endsection()
+@section('content')
+  <h1>Home Page</h1>
+@endsection
