bd8fe1fcec27c4fcb6dd8727c3e9f1dc9d6885a7
wizardy <wizardhassan99@gmail.com>
2023-10-24 13:56:20 +0100

    commit

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,17 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laravel | Test view</title>
-</head>
-<body>
-    <h1>Test view page</h1>
-    @if ($numbre > 0)
-        <h2>{{ $numbre }} is greatter than 0</h2>
-    @else
-        <h2>{{ $numbre }} is less than 0</h2>
-    @endif
-</body>
-</html>
+@extends('layout')
+@section('title')
+  Post Page
+@endsection()
+@section('content')
+  <h1>Post Page</h1>
+@endsection
