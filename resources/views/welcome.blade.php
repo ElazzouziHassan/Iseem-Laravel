48d7130ca98a3990c89edd4a57ad42d9fc1dd750
wizardy <wizardhassan99@gmail.com>
2023-10-24 13:56:34 +0100

    commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,18 +11,7 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
     </head>
     <body class="antialiased">
-        <div class="head">
-            <nav>
-                <ul>
-                    <li><a href="{{ url('/home') }}">Home</a></li>
-                    <li><a href="{{ url('/user/HASSAN') }}">User</a></li>
-                    <li><a href="{{ url('/test/20') }}">Test Page</a></li>
-                </ul>
-            </nav>
-        </div>
-        <div>
-            <h1 style="color: white; font-size:50px; font-weight:bolder">Welcome to this laravel app</h1>
-        </div>
+        
     </body>
 </html>
 
